b1ea04860cdee589ea351f7bf4af2fcfbd216988
Horppyman <horppyboi1@gmail.com>
2020-10-10 18:35:23 +0100

    Set up server

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import '@babel/polyfill';
+import express from "express";
+import dotenv from "dotenv";
+import errorHandler from "errorhandler";
+
+dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
+
+const app = express();
+
+if (!isProduction) {
+  app.use(errorHandler());
+}
+
+const PORT = process.env.PORT || 5000;
+
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    logger.error(err.stack);
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${server.address().port}`);
+});
