448267ee7fb2655853ef7032049ab59b7c7b09ae
Horppyman <horppyboi1@gmail.com>
2020-11-29 22:51:12 +0100

    (locations): user can get all locations

---
+++ src/services/locationService.js
@@ -0,0 +1,41 @@
+import database from "../database/models";
+
+const { Location, Accommodation } = database;
+
+/** Class representing LocationService */
+class LocationService {
+  /**
+   * Gets location based on its id
+   * @param {number} locationId location id should be an integer.
+   * @return {object} - location with specified id
+   */
+  static async getLocationById(locationId) {
+    try {
+      const location = await Location.findByPk(locationId);
+      return location;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Gets all locations
+   * @return {object} - an array of Location objects
+   */
+  static async getLocations() {
+    try {
+      const result = await Location.findAll({
+        include: [
+          {
+            model: Accommodations,
+          },
+        ],
+      });
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default LocationService;
