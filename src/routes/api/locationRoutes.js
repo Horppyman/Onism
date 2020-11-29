053e33ed0f691c4e97b3a4e36b561ee97b0c1403
Horppyman <horppyboi1@gmail.com>
2020-11-29 23:02:35 +0100

    Add location routes

---
+++ src/routes/api/locationRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import Location from "../../controllers/locationController";
+import method from "../../utils/method";
+
+const router = express.Router();
+
+router.route("/").get(Location.getLocations).all(method);
+
+export default router;
