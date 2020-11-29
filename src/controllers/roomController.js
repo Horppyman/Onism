a88aad2eb036083483eda78f0ab8c4edd86cef8b
Horppyman <horppyboi1@gmail.com>
2020-11-29 22:33:16 +0100

    user can get all rooms in all accommodation

---
+++ src/controllers/roomController.js
@@ -0,0 +1,24 @@
+import Response from "../utils/response";
+import accommodationService from "../services/accommodationService";
+
+/** Class that handles rooms */
+class Rooms {
+  /**
+   *  Gets all rooms
+   * @param {object} req - request object
+   * @param {object} res - response object
+   * @param {object} next - next middleware
+   * @returns {object} custom response
+   */
+  static async getAllRooms(req, res, next) {
+    try {
+      const data = await accommodationService.getAllRooms();
+      const message = "Rooms for all accommodations";
+      return Response.customResponse(res, 200, message, data);
+    } catch (error) {
+      return next(error);
+    }
+  }
+}
+
+export default Rooms;
