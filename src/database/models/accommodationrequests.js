b78a09dc412969bf74de69a15f9ef93420ff01f8
Horppyman <horppyboi1@gmail.com>
2020-12-10 09:49:13 +0100

    add database model for accommodations

---
+++ src/database/models/accommodationrequests.js
@@ -0,0 +1,25 @@
+export default (sequelize, DataTypes) => {
+  const AccommodationRequests = sequelize.define(
+    "AccommodationRequests",
+    {
+      requestId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Requests",
+          key: "id",
+        },
+      },
+      accommodationId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Accommodations",
+          key: "id",
+        },
+      },
+    },
+    {}
+  );
+  return AccommodationRequests;
+};
---
+++ src/database/models/accommodations.js
@@ -0,0 +1,61 @@
+export default (sequelize, DataTypes) => {
+  const Accommodations = sequelize.define(
+    "Accommodations",
+    {
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: "Available",
+        validate: {
+          isIn: {
+            args: [["Available", "Unavailable"]],
+            msg: "Status must be either Available or Unavailable",
+          },
+        },
+      },
+      imageUrl: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: true },
+      amenities: DataTypes.ARRAY(DataTypes.STRING),
+      locationId: { type: DataTypes.INTEGER, allowNull: false },
+      description: DataTypes.TEXT,
+      services: DataTypes.ARRAY(DataTypes.STRING),
+      owner: DataTypes.INTEGER,
+      mapLocations: DataTypes.JSONB,
+    },
+    {}
+  );
+  Accommodations.associate = (models) => {
+    Accommodations.belongsToMany(models.Requests, {
+      through: "AccommodationRequests",
+      as: "requests",
+      foreignKey: "accommodationId",
+    });
+    Accommodations.hasMany(models.Rooms, {
+      foreignKey: "accommodationId",
+      as: "rooms",
+      onDelete: "CASCADE",
+    });
+    Accommodations.hasMany(models.Ratings, {
+      foreignKey: "accommodationId",
+      as: "rating",
+      onDelete: "CASCADE",
+    });
+    Accommodations.hasMany(models.Likes, {
+      foreignKey: "accommodationId",
+      as: "likes",
+      onDelete: "CASCADE",
+    });
+    Accommodations.belongsTo(models.Location, {
+      foreignKey: "locationId",
+      onDelete: "CASCADE",
+    });
+    Accommodations.hasMany(models.Feedbacks, {
+      foreignKey: "accommodationId",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  };
+  return Accommodations;
+};
